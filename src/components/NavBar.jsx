df796ae80de057303be3043e4eff38d0cb5fc454
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-01 20:03:52 +0300

    Add LogOut btn and functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,30 @@
+import { useContext } from "react"
 import { NavLink } from "react-router-dom"
+import { AppContext } from "../context/AppContext";
+import { logoutUser } from "../services/auth.service";
 
 export default function NavBar() {
+
+    const { user, setAppState } = useContext(AppContext);
+
+    const logout = async() => {
+        await logoutUser();
+        setAppState({ user: null, userData: null})
+    };
+
     return (
         <nav>
-            <ul>
                 <NavLink to="/">Home</NavLink>
+                { user 
+            ? (
+                <>
+                    <button onClick={logout}>LogOut</button>
+                </>
+            )
+            : <>
                 <NavLink to="/login">Login</NavLink>
                 <NavLink to="/register">Register</NavLink>
-            </ul>
+            </>}
         </nav>
     )
 }
