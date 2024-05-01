2e1e8c50ebc8308e833ebe930439412949473172
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-01 16:51:08 +0300

    Add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./views/Home"
+import Login from "./views/Login"
+import Register from "./views/Register"
+import NotFound from "./views/NotFound"
 
 function App() {
 
   return (
     <>
-     
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   )
 }
