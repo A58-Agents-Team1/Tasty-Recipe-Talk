58e5542ba9b6029b1b3f51c5c1f4231d9a09b99e
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-01 16:51:36 +0300

    Add basic views for main pages

---
+++ src/views/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+        <h1>Welcome to Tasty Recipe Talk</h1>
+        <p>Here you can find and share your favorite recipes!</p>
+        </div>
+    )
+}
