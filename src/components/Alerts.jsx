cbeecbe7b331644fe3c51c67ab5facddb65df8cc
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-04 14:07:47 +0300

    add a file alerts (an alert component from chakra)

---
+++ src/components/Alerts.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Button,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogBody,
+  useDisclosure,
+    AlertDialogHeader,
+  AlertDialogFooter
+} from '@chakra-ui/react';
+import { DeleteIcon } from '@chakra-ui/icons';
+import { deletePost } from '../services/users.service';
+import { useNavigate } from 'react-router-dom';
+
+
+
+export function AlertDialogExample({postId}) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const navigate = useNavigate();
+    
+    const cancelRef = React.useRef();
+
+    function handleDelete() {
+        deletePost(postId);
+        onClose();
+    }
+    
+  return (
+    <>
+      <DeleteIcon  mt={5} onClick={onOpen} />
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Delete Customer
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='red' onClick={handleDelete} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+}
