7634514a2278ef90d65c2300447b2294f17ed98d
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-04 19:56:26 +0300

    Edit styles

--- src/hoc/Layout.jsx
+++ src/hoc/Layout.jsx
@@ -1,28 +1,27 @@
 import NavBar from "../components/NavBar.jsx";
 import Footer from "../components/Footer.jsx";
 import PropTypes from "prop-types";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function Layout({ children }) {
   return (
-    <Box bg='gray.400' height='100vh'>
+    <Box bg='gray.400' minHeight='100vh' height='100%'>
       <NavBar />
-      <Box
-        justifyContent='center'
-        m='auto'
-        p='4'
-        alignItems='center'
-        minWidth={{
-          base: "100%",
-          md: "80%",
-        }}
-        maxWidth='4xl'
-        border={`1px solid black`}
-        shadow={`lg`}
+      <Flex
+        direction='column'
+        align='center'
+        justify='center'
+        border='1px solid'
+        borderColor='gray.600'
         borderRadius='md'
+        bg='gray.300'
+        p='1rem'
+        mt='1rem'
+        mx='1rem'
+        shadow='2xl'
       >
         {children}
-      </Box>
+      </Flex>
       <Footer />
     </Box>
   );
