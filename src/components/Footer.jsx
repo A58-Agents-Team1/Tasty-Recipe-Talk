a36040bd9673daf5f7ff54cbe0fb12e1f692a81f
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-04 19:34:07 +0300

    Add styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,38 @@
 import { useEffect, useState } from "react";
-import { getAllPosts, getAllUsers } from "../services/users.service"
+import { getAllPosts, getAllUsers } from "../services/users.service";
+import { Breadcrumb, BreadcrumbItem, Flex, Text } from "@chakra-ui/react";
 export default function Footer() {
+  const [countUser, setCountUser] = useState();
+  const [countPosts, setCountPosts] = useState();
 
-    const [countUser, setCountUser] = useState();
-    const [countPosts, setCountPosts] = useState();
+  useEffect(() => {
+    getAllUsers().then((data) => setCountUser(data));
+    getAllPosts().then((data) => setCountPosts(data));
+  }, []);
 
-    useEffect(() => {
-        getAllUsers().then(data => setCountUser(data));
-        getAllPosts().then(data => setCountPosts(data));
-    }, []);
-
-
-    return (
-        <footer>
-            <p>Number of created posts: {countPosts} 📜 | All rights reserved for Tasty Recipe Talk | Number of created accounts: {countUser} 👤</p>
-        </footer>
-    )
+  return (
+    <Flex
+      as='footer'
+      position='fixed'
+      bottom='0'
+      width='100%'
+      p='5'
+      mt='4'
+      bg='gray.500'
+      justifyContent='center'
+      alignItems='center'
+    >
+      <Breadcrumb as='div'>
+        <BreadcrumbItem>
+          <Text>Number of created posts: {countPosts} 📜 </Text>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <Text> All rights reserved for Tasty Recipe Talk</Text>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <Text> Number of created accounts: {countUser} 👤</Text>
+        </BreadcrumbItem>
+      </Breadcrumb>
+    </Flex>
+  );
 }
--- src/hoc/Layout.jsx
+++ src/hoc/Layout.jsx
@@ -1,14 +1,30 @@
 import NavBar from "../components/NavBar.jsx";
 import Footer from "../components/Footer.jsx";
 import PropTypes from "prop-types";
+import { Box } from "@chakra-ui/react";
 
 export default function Layout({ children }) {
   return (
-    <div style={{ background: "#bcbcbc" }}>
+    <Box bg='gray.400' height='100vh'>
       <NavBar />
-      {children}
+      <Box
+        justifyContent='center'
+        m='auto'
+        p='4'
+        alignItems='center'
+        minWidth={{
+          base: "100%",
+          md: "80%",
+        }}
+        maxWidth='4xl'
+        border={`1px solid black`}
+        shadow={`lg`}
+        borderRadius='md'
+      >
+        {children}
+      </Box>
       <Footer />
-    </div>
+    </Box>
   );
 }
 
