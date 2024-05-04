a68711ea225879ab3e9fa52f48f74282d15308b1
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-04 18:49:59 +0300

    Rename file

---
+++ src/views/ProfileInfo.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+export default function ProfileInfo() {
+  const { userData } = useContext(AppContext);
+
+  return (
+    <div>
+      <p>Username: {userData.handle}</p>
+      <p>Email: {userData.email}</p>
+      <p>First Name: {userData.firstName}</p>
+      <p>Last Name: {userData.lastName}</p>
+    </div>
+  );
+}
