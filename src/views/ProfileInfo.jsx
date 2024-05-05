5bd413c05d0748dcedf5fa6bd01a5c6f66618110
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-05 03:28:33 +0300

    Fix Refresh problem

--- src/views/ProfileInfo.jsx
+++ src/views/ProfileInfo.jsx
@@ -1,15 +1,23 @@
-import { useContext } from "react";
-import { AppContext } from "../context/AppContext";
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProfileInfo() {
   const { userData } = useContext(AppContext);
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userData) {
+      navigate('/');
+    }
+  }, []);
   return (
     <div>
-      <p>First Name: {userData.firstName}</p>
-      <p>Last Name: {userData.lastName}</p>
-      <p>Username: {userData.handle}</p>
-      <p>Email: {userData.email}</p>
+      <p>First Name: {userData?.firstName}</p>
+      <p>Last Name: {userData?.lastName}</p>
+      <p>Username: {userData?.handle}</p>
+      <p>Email: {userData?.email}</p>
     </div>
   );
 }
