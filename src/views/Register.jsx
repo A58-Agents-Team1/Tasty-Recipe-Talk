fb077d3b8aa301542458e9161546333a0f14c760
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-05 03:18:34 +0300

    Add toast for Email UserName Success Add Styles and Chakra components

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,22 +1,31 @@
-import { useEffect, useState } from "react";
-import { registerUser } from "../services/auth.service.js";
+import { useEffect, useState } from 'react';
+import { registerUser } from '../services/auth.service.js';
 import {
   createUserHandle,
   getUserByHandle,
-} from "../services/users.service.js";
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AppContext } from "../context/AppContext.jsx";
-import { NavLink } from "react-router-dom";
-import { useToast, Button, Heading } from "@chakra-ui/react";
+} from '../services/users.service.js';
+import { Form, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContext.jsx';
+import { NavLink } from 'react-router-dom';
+import {
+  useToast,
+  Button,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Flex,
+  Box,
+} from '@chakra-ui/react';
 
 export default function Register() {
   const [form, setForm] = useState({
-    firstName: "",
-    lastName: "",
-    userName: "",
-    email: "",
-    password: "",
+    firstName: '',
+    lastName: '',
+    userName: '',
+    email: '',
+    password: '',
   });
 
   const { user, setAppState } = useContext(AppContext);
@@ -25,7 +34,7 @@ export default function Register() {
 
   useEffect(() => {
     if (user) {
-      navigate("/");
+      navigate('/');
     }
   }, [user]);
 
@@ -36,15 +45,42 @@ export default function Register() {
     });
   };
 
+  const showToastSuccess = () => {
+    toast({
+      title: 'Account created.',
+      description: "We've created your account for you.",
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+    });
+  };
+  const showToastEmailFailed = () => {
+    toast({
+      title: 'Error Account Not Created.',
+      description: 'User has already been registered!',
+      status: 'error',
+      duration: 5000,
+      isClosable: true,
+    });
+  };
+
+  const showToastUserNameFailed = () => {
+    toast({
+      title: 'Error Account Not Created.',
+      description: 'User with this username already exists!',
+      status: 'error',
+      duration: 5000,
+      isClosable: true,
+    });
+  };
+
   const register = async () => {
-    // TODO: validate form data
     try {
       const user = await getUserByHandle(form.userName);
       if (user.exists()) {
-        return console.log("User with this username already exists!");
+        return showToastUserNameFailed();
       }
       const credential = await registerUser(form.email, form.password);
-      console.log(credential.user);
       await createUserHandle(
         form.userName,
         credential.user.uid,
@@ -53,80 +89,140 @@ export default function Register() {
         form.lastName
       );
       setAppState({ user: credential.user, userData: null });
-      navigate("/");
-      showToast(); // Call function to show toast
+      showToastSuccess();
+      navigate('/');
     } catch (error) {
-      if (error.message.includes("auth/email-already-in-use")) {
-        console.log("User has already been registered!");
+      if (error.message.includes('auth/email-already-in-use')) {
+        showToastEmailFailed();
       }
     }
   };
 
-  const showToast = () => {
-    toast({
-      title: "Account created.",
-      description: "We've created your account for you.",
-      status: "success",
-      duration: 5000,
-      isClosable: true,
-    });
-  };
-
   return (
-    <div>
+    <Box width={{ base: '90%', md: '100%' }}>
       <Heading>Register</Heading>
-      <label htmlFor="firstName">First Name:</label>
-      <input
-        value={form.firstName}
-        onChange={updateForm("firstName")}
-        type="text"
-        name="firstName"
-        id="firstName"
-      />
-      <br />
-      <br />
-      <label htmlFor="lastName">Last Name:</label>
-      <input
-        value={form.lastName}
-        onChange={updateForm("lastName")}
-        type="text"
-        name="lastName"
-        id="lastName"
-      />
-      <br />
-      <br />
-      <label htmlFor="userName">User Name:</label>
-      <input
-        value={form.userName}
-        onChange={updateForm("userName")}
-        type="text"
-        name="userName"
-        id="userName"
-      />
-      <br />
-      <br />
-      <label htmlFor="email">Email:</label>
-      <input
-        value={form.email}
-        onChange={updateForm("email")}
-        type="text"
-        name="email"
-        id="email"
-      />
-      <br />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input
-        value={form.password}
-        onChange={updateForm("password")}
-        type="password"
-        name="password"
-        id="password"
-      />
-      <br />
-      <br />
-      <Button onClick={register}>Register</Button>
-      <Button><NavLink to="/login">Have an account</NavLink></Button>
-    </div>
+      <Box
+        display='grid'
+        gridTemplateColumns='repeat(1, 1fr)'
+        gap={4}
+        p={4}
+      >
+        <FormControl>
+          <FormLabel htmlFor='firstName'>First Name:</FormLabel>
+          <Input
+            value={form.firstName}
+            onChange={updateForm('firstName')}
+            type='text'
+            name='firstName'
+            placeholder='First Name'
+            id='firstName'
+            _focus={{
+              boxShadow: 'md',
+              borderRadius: 'md',
+              bg: 'gray.300',
+              p: 4,
+              transition: 'all 0.2s',
+            }}
+            bg={'gray.200'}
+            shadow={'md'}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor='lastName'>Last Name:</FormLabel>
+          <Input
+            value={form.lastName}
+            onChange={updateForm('lastName')}
+            type='text'
+            name='lastName'
+            placeholder='Last Name'
+            id='lastName'
+            _focus={{
+              boxShadow: 'md',
+              borderRadius: 'md',
+              bg: 'gray.300',
+              p: 4,
+              transition: 'all 0.2s',
+            }}
+            bg={'gray.200'}
+            shadow={'md'}
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel htmlFor='userName'>User Name:</FormLabel>
+          <Input
+            value={form.userName}
+            onChange={updateForm('userName')}
+            type='text'
+            name='userName'
+            placeholder='Username'
+            id='userName'
+            _focus={{
+              boxShadow: 'md',
+              borderRadius: 'md',
+              bg: 'gray.300',
+              p: 4,
+              transition: 'all 0.2s',
+            }}
+            bg={'gray.200'}
+            shadow={'md'}
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel htmlFor='email'>Email:</FormLabel>
+          <Input
+            value={form.email}
+            onChange={updateForm('email')}
+            type='text'
+            name='email'
+            placeholder='Email'
+            id='email'
+            _focus={{
+              boxShadow: 'md',
+              borderRadius: 'md',
+              bg: 'gray.300',
+              p: 4,
+              transition: 'all 0.2s',
+            }}
+            bg={'gray.200'}
+            shadow={'md'}
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel htmlFor='password'>Password:</FormLabel>
+          <Input
+            value={form.password}
+            onChange={updateForm('password')}
+            type='password'
+            name='password'
+            placeholder='Password'
+            id='password'
+            _focus={{
+              boxShadow: 'md',
+              borderRadius: 'md',
+              bg: 'gray.300',
+              p: 4,
+              transition: 'all 0.2s',
+            }}
+            bg={'gray.200'}
+            shadow={'md'}
+          />
+        </FormControl>
+      </Box>
+      <Flex
+        justify='center'
+        align='center'
+        gap={4}
+      >
+        <Button
+          colorScheme='blue'
+          onClick={register}
+        >
+          Create
+        </Button>
+        <Button colorScheme='green'>
+          <NavLink to='/login'>Login</NavLink>
+        </Button>
+      </Flex>
+    </Box>
   );
 }
