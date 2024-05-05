5894d12550c210dec26f3804340cf63e0984ff8b
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-06 01:09:27 +0300

    Add constants

---
+++ src/common/constants.js
@@ -0,0 +1,15 @@
+export const MIN_FIRST_NAME_LENGTH = 4;
+
+export const MAX_FIRST_NAME_LENGTH = 15;
+
+export const MIN_LAST_NAME_LENGTH = 4;
+
+export const MAX_LAST_NAME_LENGTH = 15;
+
+export const MIN_USERNAME_LENGTH = 3;
+
+export const MAX_USERNAME_LENGTH = 15;
+
+export const MIN_PASSWORD_LENGTH = 6;
+
+export const MAX_PASSWORD_LENGTH = 15;
