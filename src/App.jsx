a2d301b7b33e08e95433adee10fa8094d4f417af
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-05 03:28:54 +0300

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { AppContext } from "./context/AppContext.jsx";
-import { getUserData } from "./services/users.service.js";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./config/firebase-config.js";
-import { CreatePost } from "./views/CreatePost.jsx";
-import Layout from "./hoc/Layout.jsx";
-import Home from "./views/Home.jsx";
-import Login from "./views/Login.jsx";
-import Register from "./views/Register.jsx";
-import NotFound from "./views/NotFound.jsx";
-import AllPosts from "./views/AllPosts.jsx";
-import About from "./views/About.jsx";
-import BlockedAccounts from "./views/BlockedAccounts.jsx";
-import ProfileInfo from "./views/ProfileInfo.jsx";
-import FullViewRecipe from "./views/FullViewRecipe.jsx";
+import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { AppContext } from './context/AppContext.jsx';
+import { getUserData } from './services/users.service.js';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './config/firebase-config.js';
+import { CreatePost } from './views/CreatePost.jsx';
+import Layout from './hoc/Layout.jsx';
+import Home from './views/Home.jsx';
+import Login from './views/Login.jsx';
+import Register from './views/Register.jsx';
+import NotFound from './views/NotFound.jsx';
+import AllPosts from './views/AllPosts.jsx';
+import About from './views/About.jsx';
+import BlockedAccounts from './views/BlockedAccounts.jsx';
+import ProfileInfo from './views/ProfileInfo.jsx';
+import FullViewRecipe from './views/FullViewRecipe.jsx';
 
 function App() {
   const [appState, setAppState] = useState({
@@ -32,7 +32,6 @@ function App() {
     if (!appState.user) return;
 
     getUserData(appState.user.uid).then((snapshot) => {
-      //console.log(snapshot.val()); // { Peter: {...} }
       const userData = Object.values(snapshot.val())[0];
       setAppState({ ...appState, userData });
     });
@@ -43,16 +42,46 @@ function App() {
       <AppContext.Provider value={{ ...appState, setAppState }}>
         <Layout>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='*' element={<NotFound />} />
-            <Route path='/create-post' element={<CreatePost />} />
-            <Route path='/all-posts' element={<AllPosts />} />
-            <Route path='/posts/:id' element={<FullViewRecipe />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/blocked-accounts' element={<BlockedAccounts />} />
-            <Route path='/my-profile' element={<ProfileInfo />} />
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            <Route
+              path='/login'
+              element={<Login />}
+            />
+            <Route
+              path='/register'
+              element={<Register />}
+            />
+            <Route
+              path='*'
+              element={<NotFound />}
+            />
+            <Route
+              path='/create-post'
+              element={<CreatePost />}
+            />
+            <Route
+              path='/all-posts'
+              element={<AllPosts />}
+            />
+            <Route
+              path='/posts/:id'
+              element={<FullViewRecipe />}
+            />
+            <Route
+              path='/about'
+              element={<About />}
+            />
+            <Route
+              path='/blocked-accounts'
+              element={<BlockedAccounts />}
+            />
+            <Route
+              path='/my-profile'
+              element={<ProfileInfo />}
+            />
           </Routes>
         </Layout>
       </AppContext.Provider>
