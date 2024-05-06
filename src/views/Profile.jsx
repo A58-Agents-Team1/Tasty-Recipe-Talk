8829a6c2aa62cac10181538eafe2fbdd84bc50ea
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-06 18:11:07 +0300

    Refactor

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,21 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export const Profile = ({ children }) => {
-  const { userData } = useContext(AppContext);
+  const { user, userData } = useContext(AppContext);
+  const navigate = useNavigate();
 
-  if (userData.userRole === 'admin') {
-    return <>{children}</>;
-  }
+  useEffect(() => {
+    !userData && navigate('/login');
+  }, []);
 
   return (
-    <div>
-      <h1>Hello, {userData.handle}, what recipe will you upload today?</h1>
-      <p>Username: {userData.handle}</p>
-      <p>Email: {userData.email}</p>
-      <p>First Name: {userData.firstName}</p>
-      <p>Last Name: {userData.lastName}</p>
-      <br />
-    </div>
+    <>
+      {userData && (
+        <>
+          {userData.userRole === 'admin' && <>{children}</>}
+          {userData.userRole !== 'admin' && (
+            <>
+              <h1>
+                Hello, {userData.handle}, what recipe will you upload today?
+              </h1>
+              <p>Username: {userData.handle}</p>
+              <p>Email: {userData.email}</p>
+              <p>First Name: {userData.firstName}</p>
+              <p>Last Name: {userData.lastName}</p>
+              <br />
+            </>
+          )}
+        </>
+      )}
+    </>
   );
 };
