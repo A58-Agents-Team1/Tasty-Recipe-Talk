8aa8858b2c980e7e44cedcbbc0b1b104e364ff1a
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-06 18:14:58 +0300

    Refactor

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -1,22 +1,26 @@
-import { useContext, useEffect, useState } from "react";
-import { AppContext } from "../context/AppContext";
-import { useSearchParams } from "react-router-dom";
-import { blockAccount, getUserByHandle,getUsersByEmail, getUsersByName, unblockAccount } from "../services/users.service";
-import { Button, useToast } from "@chakra-ui/react";
-import { showToast } from "../components/Alerts";
-
+import { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import { useSearchParams } from 'react-router-dom';
+import {
+  blockAccount,
+  getUserByHandle,
+  getUsersByEmail,
+  getUsersByName,
+  unblockAccount,
+} from '../services/users.service';
+import { Button, useToast } from '@chakra-ui/react';
+import { showToast } from '../components/Alerts';
 
 export default function Admin() {
   const { userData } = useContext(AppContext);
   const [users, setUsers] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const search = searchParams.get("search") || "";
+  const search = searchParams.get('search') || '';
   const toast = useToast();
 
   const setSearch = (value) => {
     setSearchParams({ search: value });
   };
-
 
   useEffect(() => {
     const fetchAccount = async () => {
@@ -24,7 +28,7 @@ export default function Admin() {
         const resultWithHandle = await getUserByHandle(search);
         const resultWithEmail = await getUsersByEmail(search);
         const resultWithName = await getUsersByName(search);
-        
+
         if (resultWithHandle.exists()) {
           const userHandle = resultWithHandle.val();
           setUsers([userHandle]);
@@ -36,46 +40,48 @@ export default function Admin() {
           setUsers([]);
         }
       } catch (error) {
-        console.error("Error fetching user data:", error);
+        console.error('Error fetching user data:', error);
       }
     };
     fetchAccount();
-   
-  }, [search,users]);
+  }, [search, users]);
 
   const blockAcc = (inputData) => {
     try {
       blockAccount(inputData);
-      console.log("yes");
-      showToast("Account block.", "Account blocked successfully", toast);
+      console.log('yes');
+      showToast('Account block.', 'Account blocked successfully', toast);
     } catch (error) {
       console.log(error.message);
     }
-   
-  }
-  
+  };
+
   const unblockAcc = (inputData) => {
     try {
       unblockAccount(inputData);
-      console.log("yes");
-      showToast("Account unblock.", "Account unblocked successfully", toast);
+      console.log('yes');
+      showToast('Account unblock.', 'Account unblocked successfully', toast);
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   return (
     <div>
-      <input type="text"
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      name="search"
-      id="search"/>
+      <input
+        type='text'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        name='search'
+        id='search'
+      />
       <p>Username: {userData.handle}</p>
       <p>Email: {userData.email}</p>
       <p>First Name: {userData.firstName}</p>
-      <p>Last Name: {userData.lastName}</p><br />
-        {users.map((user) => (
+      <p>Last Name: {userData.lastName}</p>
+      <br />
+      {search &&
+        users.map((user) => (
           <div key={user.uid}>
             <h1>Results :</h1>
             <p>Username : {user.handle}</p>
@@ -83,12 +89,14 @@ export default function Admin() {
             <p>last name : {user.lastName}</p>
             <p>Email : {user.email}</p>
             {user.isBlocked ? (
-            <Button onClick={() => unblockAcc(user.handle)}>Unblock user</Button>
-          ) : (
-            <Button onClick={() => blockAcc(user.handle)}>Block user</Button>
-          )}
+              <Button onClick={() => unblockAcc(user.handle)}>
+                Unblock user
+              </Button>
+            ) : (
+              <Button onClick={() => blockAcc(user.handle)}>Block user</Button>
+            )}
           </div>
-      ))}
+        ))}
     </div>
   );
 }
