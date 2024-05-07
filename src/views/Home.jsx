347c339f98553d123b0de078fe26e4bd7b73bdc6
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-07 20:14:58 +0300

    Show 5 posts on home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,25 @@
 import { Heading, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { getAllPosts } from "../services/posts.service";
+import Post from "../components/Post";
+
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getAllPosts('').then(setPosts);
+  }, []);
+
   return (
     <div>
       <Heading>Welcome to Tasty Recipe Talk</Heading>
       <Text>Here you can find and share your favorite recipes!</Text>
+      {posts && (
+        posts.slice(0, 5).map((post) => (
+          <Post key={post.id} post={post} />
+        ))
+      )}
     </div>
   );
 }
