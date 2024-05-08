11f110a764f79c591918b038c3e63560da853a4c
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-05-08 14:36:11 +0300

    Fix Search bar problem

--- src/views/AllPosts.jsx
+++ src/views/AllPosts.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
-import Post from "../components/Post";
-import { useSearchParams } from "react-router-dom";
-import { onChildChanged, ref } from "firebase/database";
-import { db } from "../config/firebase-config";
-import { getAllPosts } from "../services/posts.service";
-import { Heading } from "@chakra-ui/react";
+import { useEffect, useState } from 'react';
+import Post from '../components/Post';
+import { useSearchParams } from 'react-router-dom';
+import { onChildChanged, ref } from 'firebase/database';
+import { db } from '../config/firebase-config';
+import { getAllPosts } from '../services/posts.service';
+import { Box, Heading } from '@chakra-ui/react';
 
 export default function AllPosts() {
   const [posts, setPosts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const search = searchParams.get("search") || "";
+  const search = searchParams.get('search') || '';
 
   const setSearch = (value) => {
     setSearchParams({ search: value });
@@ -20,7 +20,7 @@ export default function AllPosts() {
   }, [search]);
 
   useEffect(() => {
-    return onChildChanged(ref(db, "posts"), (snapshot) => {
+    return onChildChanged(ref(db, 'posts'), (snapshot) => {
       const value = snapshot.val();
       setPosts((posts) =>
         posts.map((post) => {
@@ -40,28 +40,27 @@ export default function AllPosts() {
   }, []);
 
   return (
-    <div>
+    <Box w={'100%'}>
+      <Heading textAlign={'center'}>All recipes: </Heading>
+      <label htmlFor='search'>Search: </label>
+      <input
+        title='Search for a recipe'
+        type='text'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        name='search'
+        id='search'
+      />
+      <br />
       {posts.length !== 0 ? (
         <>
-          <Heading>All recipes: </Heading>
-          <label htmlFor="search">Search</label>
-          <br />
-          <input
-            type="text"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            name="search"
-            id="search"
-          />
-          <br />
           {posts.map((post) => (
             <Post key={post.id} post={post} />
           ))}
         </>
       ) : (
         <Heading>No posts found</Heading>
-      )
-      }
-    </div>
+      )}
+    </Box>
   );
 }
