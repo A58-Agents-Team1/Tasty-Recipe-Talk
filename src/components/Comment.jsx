75258fc06e7e4430c8c9132d72ef8435b672ef2e
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-09 16:10:15 +0300

    Add functionality for <Comment />

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,16 +1,50 @@
 import {
+  Avatar,
   Box,
+  Button,
   Card,
   CardBody,
   CardHeader,
+  Flex,
   Heading,
+  Icon,
+  Input,
+  Spacer,
+  Spinner,
   Stack,
   StackDivider,
   Text,
 } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
+import { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import { EditIcon } from '@chakra-ui/icons';
+import { updateComment } from '../services/posts.service';
 
-export default function Comment({ comments }) {
+export default function Comment({ comments, postId }) {
+  const { userData } = useContext(AppContext);
+
+  const [newComment, setNewComment] = useState('');
+  const [editComment, setEditComment] = useState('');
+  const [currentComment, setCurrentComment] = useState('');
+
+  const handleEditToggle = (content) => {
+    setEditComment((prev) => !prev);
+    setCurrentComment(content);
+    setNewComment(content);
+  };
+
+  const handleEditComment = async (id, commentId, content) => {
+    await updateComment(id, commentId, content);
+    setEditComment(false);
+    setCurrentComment('');
+  };
+
+  const handleCancelComment = () => {
+    setEditComment(false);
+    setCurrentComment('');
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -25,19 +59,90 @@ export default function Comment({ comments }) {
           {comments.length !== 0 ? (
             comments.map((comment) => (
               <Box key={comment.id}>
-                {console.log('FROM COMMENT:', comment.id)}
                 <Heading
                   size='xs'
                   textTransform='uppercase'
+                  display='flex'
+                  flexDirection={{ base: 'column-reverse', md: 'row-reverse' }}
                 >
-                  {comment.author}
+                  <Avatar
+                    size='sm'
+                    name={comment.author ? comment.author : ''}
+                    src={comment.authorPicture ? userData.avatar : ''}
+                  />
+                  <Text
+                    as='div'
+                    m={2}
+                  >
+                    {comment.author ? comment.author : <Spinner />}
+                  </Text>
                 </Heading>
-                <Text
-                  pt='2'
-                  fontSize='sm'
+
+                {editComment && currentComment === comment.content ? (
+                  <Input
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}
+                  />
+                ) : (
+                  <Text
+                    pt='2'
+                    fontSize='lg'
+                  >
+                    {comment.content}
+                  </Text>
+                )}
+
+                <Flex
+                  align='end'
+                  justify='center'
                 >
-                  {comment.content}
-                </Text>
+                  {comment.createdOn !== comment.lastEdited && (
+                    <Text
+                      fontWeight='500'
+                      mt='4'
+                    >
+                      Last Edit:{' '}
+                      {new Date(comment.lastEdited).toLocaleDateString()}
+                    </Text>
+                  )}
+                  <Text
+                    fontWeight='500'
+                    mt='4'
+                  >
+                    Post Created:{' '}
+                    {new Date(comment.createdOn).toLocaleDateString()}
+                  </Text>
+                  <Spacer />
+                  {userData.handle === comment.author && (
+                    <>
+                      {editComment && currentComment === comment.content ? (
+                        <Box mt={2}>
+                          <Button
+                            mx={2}
+                            onClick={() =>
+                              handleEditComment(postId, comment.id, newComment)
+                            }
+                          >
+                            Edit
+                          </Button>
+                          <Button onClick={() => handleCancelComment()}>
+                            Cancel
+                          </Button>
+                        </Box>
+                      ) : (
+                        <Icon
+                          aria-label='Edit Icon'
+                          as={EditIcon}
+                          onClick={() => {
+                            handleEditToggle(comment.content);
+                          }}
+                          boxSize={6}
+                          cursor={'pointer'}
+                        />
+                      )}
+                    </>
+                  )}
+                </Flex>
               </Box>
             ))
           ) : (
