ddda1fd84eec118b6d128ee392d20b32011ce6d8
TanyaZhecheva <jechevatanq@gmail.com>
2024-05-10 17:48:33 +0300

    add tabs in all recipes to sort by

--- src/views/AllPosts.jsx
+++ src/views/AllPosts.jsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from 'react';
 import Post from '../components/Post';
-import { useSearchParams } from 'react-router-dom';
+import { Form, useSearchParams } from 'react-router-dom';
 import { onChildChanged, ref } from 'firebase/database';
 import { db } from '../config/firebase-config';
 import { getAllPosts } from '../services/posts.service';
-import { Box, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  FormLabel,
+  Heading,
+  Input,
+  Tab,
+  TabList,
+  Tabs,
+} from '@chakra-ui/react';
 
 export default function AllPosts() {
   const [posts, setPosts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const search = searchParams.get('search') || '';
+  const [sortBy, setSortBy] = useState('createdOn');
 
   const setSearch = (value) => {
     setSearchParams({ search: value });
@@ -37,26 +47,62 @@ export default function AllPosts() {
         })
       );
     });
-  }, []);
+  }, [sortBy]);
 
   return (
     <Box w={'100%'}>
-      <Heading textAlign={'center'}>All recipes: </Heading>
-      <label htmlFor='search'>Search: </label>
-      <input
-        title='Search for a recipe'
-        type='text'
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        name='search'
-        id='search'
-      />
+      <Flex mt={5} align={'center'} justify={'center'}>
+        <FormLabel fontWeight={800} fontSize={30} htmlFor='search'>
+          Search:{' '}
+        </FormLabel>
+        <Input
+          title='Search for a recipe'
+          type='text'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          name='search'
+          id='search'
+          backgroundColor='white'
+          w={'30%'}
+        />
+      </Flex>
       <br />
       {posts.length !== 0 ? (
         <>
-          {posts.map((post) => (
-            <Post key={post.id} post={post} />
-          ))}
+          <Tabs variant='enclosed' color={'white'}>
+            <TabList>
+              <Tab
+                _selected={{ color: 'white', bg: 'blue.500' }}
+                onClick={() => setSortBy('comments')}
+              >
+                by comments
+              </Tab>
+              <Tab
+                _selected={{ color: 'white', bg: 'red.500' }}
+                onClick={() => setSortBy('likedBy')}
+              >
+                by likes
+              </Tab>
+              <Tab
+                _selected={{ color: 'white', bg: 'orange.500' }}
+                onClick={() => setSortBy('createdOn')}
+              >
+                created on
+              </Tab>
+            </TabList>
+          </Tabs>
+          {posts
+            .filter((post) => post[sortBy] !== undefined)
+            .sort((a, b) => {
+              const keyA = Object.values(a[sortBy]).length;
+              const keyB = Object.values(b[sortBy]).length;
+              if (keyA < keyB) return 1;
+              if (keyA > keyB) return -1;
+              return 0;
+            })
+            .map((post) => (
+              <Post key={post.id} post={post} />
+            ))}
         </>
       ) : (
         <Heading>No posts found</Heading>
