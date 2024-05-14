aed4423720ffd1bd4d0954c9c37eb131a6b1c528
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2024-05-14 22:16:02 +0300

    Add button for unblock

--- src/views/BlockedAccounts.jsx
+++ src/views/BlockedAccounts.jsx
@@ -1,13 +1,27 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Heading, Text, useToast } from '@chakra-ui/react';
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
-import { getAllUsersByIsBlocked } from '../services/users.service';
+import {
+  getAllUsersByIsBlocked,
+  unblockAccount,
+} from '../services/users.service';
+import { showToast, showToastError } from '../components/Alerts';
 
 export default function BlockedAccounts() {
   const { userData } = useContext(AppContext);
+  const toast = useToast();
   const navigate = useNavigate();
   const [allBlockedUsers, setAllBlockedUsers] = useState([]);
+
+  const unblockAcc = (inputData) => {
+    try {
+      unblockAccount(inputData);
+      showToast('Account unblock.', 'Account unblocked successfully', toast);
+    } catch (error) {
+      showToastError('Error unblocking account', error.message, toast);
+    }
+  };
 
   useEffect(() => {
     const fetchBlockedUsers = async () => {
@@ -16,7 +30,7 @@ export default function BlockedAccounts() {
     };
     !userData && navigate('/login');
     fetchBlockedUsers();
-  }, []);
+  }, [allBlockedUsers]);
 
   return (
     <Box
@@ -47,7 +61,11 @@ export default function BlockedAccounts() {
       <>
         {allBlockedUsers ? (
           allBlockedUsers.map((user) => (
-            <Box key={user.uid} fontWeight={'bold'} fontSize={'lg'}>
+            <Box
+              key={user.uid}
+              fontWeight={'bold'}
+              fontSize={'lg'}
+            >
               <Box
                 p={4}
                 m={3}
@@ -61,6 +79,23 @@ export default function BlockedAccounts() {
                 <Text>Last Name: {user.lastName}</Text>
                 <Text>Username: {user.handle}</Text>
                 <Text>Email: {user.email}</Text>
+                {user.isBlocked ? (
+                  <Button
+                    mt={2}
+                    colorScheme='green'
+                    onClick={() => unblockAcc(user?.handle)}
+                  >
+                    Unblock user
+                  </Button>
+                ) : (
+                  <Button
+                    mt={2}
+                    colorScheme='red'
+                    onClick={() => blockAcc(user?.handle)}
+                  >
+                    Block user
+                  </Button>
+                )}
               </Box>
             </Box>
           ))
